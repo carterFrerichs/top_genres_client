fe0a01827ed8cd7f9f19116b67fdee9f1be89c56
Carter Frerichs <60456232+carterFrerichs@users.noreply.github.com>
2021-03-23 15:16:19 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'; 
 import './App.css';
+import SpotifyWebApi from 'spotify-web-api-js';
+const spotifyApi = new SpotifyWebApi();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component { 
+  constructor(){
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      topGenres: { genres: [], count: 0 }
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+  getTopGenres(){
+    spotifyApi.getMyTopArtists({limit: 50})
+      .then((response) => {
+
+        console.log(response);
+        // this.setState({
+        //   nowPlaying: { 
+        //       name: response.item.name, 
+        //       albumArt: response.item.album.images[0].url
+        //     }
+        // });
+      })
+  }
+  render() {
+    return (
+      <div className='App'>
+        <a href='http://localhost:8888/login'> Login to Spotify </a>
+        <button onClick={this.getTopGenres()}></button>
+      </div>
+    )
+  }
 }
-
-export default App;
